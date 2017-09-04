24ce83c62b31f97f602fc5da695363e449243eda
alexsmith <alexsmith@10.30.22.66>
2017-09-03 19:37:46 -0500

    Changed output to be displayed using out.println() in JSP page.

---
+++ src/java/model/WelcomeService.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author alexsmith
+ */
+public class WelcomeService {
+    private Calendar now = Calendar.getInstance();
+   
+    private String getTimeOfDay(){
+        int currentHour = now.get(Calendar.HOUR_OF_DAY);
+        String timeOfDay;
+
+        if (currentHour >= 5 && currentHour < 12) {
+            timeOfDay = "Morning";
+
+        } else if (currentHour >= 12 && currentHour < 8) {
+            timeOfDay = "Afternoon";
+        } else {
+            timeOfDay = "Evening";
+        }
+
+        return timeOfDay;
+    }
+    
+    public String getGreeting(String name){
+        String greeting = "Good " + getTimeOfDay() + " " + name + "!";
+        
+        return greeting;
+    }
+}
